63f58d4a827bbef54ec1cd300bb2c1dc102d6dc9
DianaKhTs <111983177+DianaKhTs@users.noreply.github.com>
2023-11-09 17:30:30 +0300

    first commit

---
+++ sem2_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include "string.h"
+#include "queue"
+
+using namespace std;
+
+int main(){
+    string st;
+    cin>>st;
+    string str;
+    cin>>str;
+    queue<char> q;
+
+    for (int i = 0; i < st.size();i++){
+        q.push(st[i]);
+    }
+
+    for (int i = 0; i < str.size();i++){
+        if (str[i] == q.front() ){
+            q.pop();
+        }
+
+
+    }
+    if (q.size()==0){
+        cout<<"yes";
+    }
+    
+    return 0;
+}
